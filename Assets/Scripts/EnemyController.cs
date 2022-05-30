d7cfaf88afd358f4c986314a2342776d523ce7d2
algatrazcpt <53114980+algatrazcpt@users.noreply.github.com>
2022-05-30 16:45:22 +0300

    version1

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float health = 50f;
+    public float damage = 10f;
+    public float attackRange=0.7f;
+    public float enemyTrigger = 1.7f;
+    private float speed = 5f;
+    public Animator animator;
+    public Transform playerLocation;
+    public LayerMask playerLayer;
+    public float moveDistance = 0.5f;
+    public float heightDistance = 0.5f;
+    Rigidbody2D rigidbody2d;
+    bool playerFollow = false;
+    void Start()
+    {
+        playerLayer = LayerMask.GetMask("Player");
+        rigidbody2d=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyTrigger();
+        if(playerFollow)
+        {
+            PlayerFollow();
+        }
+        
+    }
+    void EnemyTrigger()
+    {
+        var detection = Physics2D.OverlapCircle(transform.position, enemyTrigger, playerLayer);
+        if(detection!=null)
+        {
+            playerFollow = true;
+        }
+    }
+    bool attackAble = true;
+    float attackRate = 1f;
+    float currentTime = 0f;
+    void PlayerFollow()
+    {
+        float playerDistanceX = Vector3.Distance(new Vector3(transform.position.x,0,0) ,new Vector3(playerLocation.position.x,0,0));
+        float playerDistanceY = Vector3.Distance(new Vector3(0, transform.position.y, 0), new Vector3(0, playerLocation.position.y, 0));
+        if(playerDistanceX > moveDistance)
+        {
+            Vector2 target=new Vector2(playerLocation.position.x, transform.position.y);
+            Vector2 move =Vector2.MoveTowards(rigidbody2d.position, target,speed*Time.deltaTime);
+            rigidbody2d.MovePosition(new Vector2(move.x,transform.position.y));
+            Debug.Log(playerDistanceX);
+        }
+        Debug.Log(moveDistance);
+        if (playerDistanceX < moveDistance && playerDistanceY < heightDistance) 
+        {
+            EnemyAttack();
+        }
+    }
+    void EnemyAttack()
+    {
+            if (Time.time > currentTime)
+            {
+                currentTime = Time.time + 3.5f / attackRate;
+                animator.SetTrigger("attackT");
+            }
+    }
+    public void TakeDamage(float value)
+    {
+        animator.SetTrigger("takeDamageT");
+        health-=value;
+        if(health<=0)
+        {
+            Death();
+        }
+    }
+    void Death()
+    {
+        animator.SetTrigger("deathT");
+        Destroy(gameObject);
+    }
+    public void Attack()
+    {
+        var detection = Physics2D.OverlapCircle(transform.position, attackRange, playerLayer);
+        if (detection != null)
+        {
+            detection.gameObject.GetComponent<PlayerControl>().TakeDamage(damage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Animator animator;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float damage = 10f;
+    public float health = 100f;
+    public float attackRange = 0.1f;
+    public float offsetX = 0.5f;
+    public LayerMask enemyLayer;
+    Rigidbody2D _rigidbody;
+    public Transform attackPosition;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        enemyLayer = LayerMask.GetMask("Enemy");
+    }
+    void Update()
+    {
+        PlayerMove();
+        PlayerJump();
+        Attack();
+    }
+    void PlayerMove()
+    {
+        float xAxis = Input.GetAxis("Vertical");
+        float yAxis = Input.GetAxis("Horizontal");
+        if (yAxis != 0)
+        {
+            transform.position+=(new Vector3(yAxis,0)*moveSpeed*Time.deltaTime);
+            transform.rotation = yAxis < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+            animator.SetBool("RunB", true);
+        }
+        else
+        {
+            animator.SetBool("RunB", false);
+        }
+    }
+    bool isDoubleJump = true;
+    bool isGround = true;
+    bool isJump = false;
+    void PlayerJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGround==false&&isDoubleJump||isGround)
+            {
+                _rigidbody.AddForce(new Vector2(0, jumpForce),ForceMode2D.Impulse);
+                isDoubleJump = isGround;
+                animator.SetTrigger("JumpStartT");
+                isJump = true;
+            }
+            isGround = false;
+        }
+    }
+    void Attack()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("AttackT");
+        }
+    }
+    public void PlayerAttack()
+    {
+        Collider2D [] enemys = Physics2D.OverlapCircleAll(new Vector2(attackPosition.position.x,attackPosition.position.y), attackRange,enemyLayer);
+        foreach(var enemy in enemys)
+        {
+            enemy.gameObject.GetComponent<EnemyController>().TakeDamage(damage);
+        }
+    }
+    public void TakeDamage(float value)
+    {
+        animator.SetTrigger("TakeDamageT");
+        health-=value;
+        if(health<=0)
+        {
+            PlayerDeath();
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+    }
+    void PlayerDeath()
+    {
+        Debug.Log("Player death");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isDoubleJump = true;
+            isGround = true;
+            if(isJump)
+            {
+                animator.SetTrigger("JumpFinishT");
+            }
+        }
+    }
+}
